4a0b3f87b8762a05db1aa97e2de3cebde21e0f5a
gabylinda408 <gabriely.semianko@escola.pr.gov.br>
2025-05-25 19:47:16 +0000

    aula 10

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,33 @@ criaCartao(
     'Quem foi o líder da seita Heaven’s Gate?',
     'O líder da seita Heaven’s Gate foi Marshall Applewhite, que acreditava que a Terra estava prestes a ser destruída e que seus seguidores precisavam deixar seus corpos físicos para alcançar um nível superior de existência.'
 ) 
+criaCartao(
+    'História',
+    'Qual foi a maior guerra da história?',
+    'A maior guerra da história foi a Segunda Guerra Mundial, que ocorreu de 1939 a 1945 e envolveu a maioria das nações do mundo, resultando em milhões de mortes e mudanças geopolíticas significativas.'
+)
+criaCartao(
+    'Seitas',
+    'O que foi Rajneeshpuram?',
+    'Rajneeshpuram foi uma comunidade utópica fundada pelo guru Bhagwan Shree Rajneesh (Osho) nos anos 80 em Oregon, EUA. A seita ficou conhecida por suas práticas controversas e conflitos com as autoridades locais.'
+)
+criaCartao(
+    'História',
+    'Qual foi a maior civilização da história?',
+    'A maior civilização da história é frequentemente considerada a civilização romana, que se estendeu por grande parte da Europa, Norte da África e partes do Oriente Médio, influenciando profundamente a cultura, política e direito ocidentais.'
+)
+criaCartao(
+    'Seitas',
+    'Quem foi o líder da seita Branch Davidians?',
+    'O líder da seita Branch Davidians foi David Koresh, que acreditava ser o Messias e liderou seus seguidores em um cerco de 51 dias com o FBI em Waco, Texas, em 1993.'
+)
+criaCartao(
+    'História',
+    'Qual foi a maior batalha da história?',
+    'A maior batalha da história é frequentemente considerada a Batalha de Stalingrado, que ocorreu durante a Segunda Guerra Mundial entre 1942 e 1943, resultando em milhões de mortes e marcando um ponto de virada crucial na guerra.'
+)
+criaCartao(
+    'Seitas',
+    'Quem foi Chasles Manson?',
+    'Charles Manson foi o líder de uma seita conhecida como "Família Manson", que ficou infame por seus assassinatos de celebridades e outros crimes nos anos 60, incluindo o assassinato de Sharon Tate e outros em 1969.'
+)
