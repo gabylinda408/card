0c71a8da74627b01b7393a68ef4af33e02f47196
gabylinda408 <gabriely.semianko@escola.pr.gov.br>
2025-05-25 18:43:44 +0000

    aula 6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    "História",
+    'Qual foi a pandemia mais letal de todos os tempos?',
+    'A pandemia mais letal de todos os tempos foi a Peste Negra, que ocorreu no século XIV e matou cerca de 75 a 200 milhões de pessoas na Europa, Ásia e África.'
+)
+
+criaCartao(
+    'Seitas',
+    'Quem foi o líder do Ramo Davidiano?',
+    'O líder do Ramo Davidiano foi David Koresh, que se autodenominava o "Cordeiro de Deus" e acreditava ser o profeta escolhido para interpretar os selos do Apocalipse.'
+)
+
+criaCartao(
+    'História',
+   'Qual foi o maior imperador do mundo?',
+    'O maior imperador do mundo foi Genghis Khan, que fundou o Império Mongol no século XIII, o maior império contíguo da história, abrangendo vastas áreas da Ásia e Europa.'
+)
